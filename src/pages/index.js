25d0ee04ea88f5c84161b689f0c41fb126c15e99
sajjad ali <devsajjad5@gmail.com>
2023-03-04 07:50:32 +0300

    setting new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>IQ Bazar</title>
-        <meta name="description" content="IQ Bazar E-commerce" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <>
+        <Head>
+          <title>IQ Bazar</title>
+          <meta name="description" content="IQ Bazar E-commerce" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+      </>
+
+      
     </>
   );
 }
