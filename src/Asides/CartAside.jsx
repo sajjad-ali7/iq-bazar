fee5a32308ad1e15dbb4040794a08f4a458013f7
sajjad ali <devsajjad5@gmail.com>
2023-05-27 08:21:04 +0300

    fixes

--- src/Asides/CartAside.jsx
+++ src/Asides/CartAside.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { AiOutlineClose } from "react-icons/ai";
-import emptyBag from "/Bag.json";
+import emptyBag from "C:/Users/hp/Documents/iq-bazar/public/Bag.json";
 import Lottie from "lottie-react-web";
 import { FiShoppingBag } from "react-icons/fi";
 import { CART_ASIDE_INDEX, CART_ITEMS } from "@/consts";
